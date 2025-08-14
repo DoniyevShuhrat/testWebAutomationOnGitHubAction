c63740db8eecc086b60fb92b24a2339c4e8218d4
DoniyevShuhrat <doniyevshuhrat96304@gmail.com>
2025-08-15 01:14:25 +0500

    changed test_example_site file

--- tests/test_example_site.py
+++ tests/test_example_site.py
@@ -1,15 +1,28 @@
+from asyncio import sleep
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.example_page import ExamplePage
 
-def test_title_is_example_domain(driver):
-    page = ExamplePage(driver)
-    page.open()
-    assert driver.title == "Example Domain"
+def test_title_is_example_domain():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.chrome.service import Service
 
-def test_more_information_navigates_to_more_info_page(driver):
-    page = ExamplePage(driver)
-    page.open()
-    page.click_more_info()
-    WebDriverWait(driver, 10).until(EC.title_is("More Information | Example Domain"))
-    assert "IANA" in driver.title
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        driver.get("https://texnomart.uz")
+        assert driver.title == "Texnomart.uz | Ўзбекистонда маиший техника ва электроника онлайн дўкони"
+        sleep(5)
+    finally:
+        driver.quit()
+
+
+# def test_more_information_navigates_to_more_info_page(driver):
+#     page = ExamplePage(driver)
+#     page.open()
+#     page.click_more_info()
+#     WebDriverWait(driver, 10).until(EC.title_is("Texnomart.uz"))
+#     assert "Texnomart.uz" in driver.title
